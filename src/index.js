a15bbe00461d011b13fc3660fbeee4dbe09e8761
Bontle <126960560+bontlenkoale1@users.noreply.github.com>
2024-08-24 16:58:48 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ function search(event) {
   let searchInputElement = document.querySelector("#search-input");
   let city = searchInputElement.value;
 
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "0fd2f6d04c480da7a695db3eo9b870t6";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayTemperature);
